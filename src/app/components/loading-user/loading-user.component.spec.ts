c0c01e55a05950d7488199e7d83e3425fe50c4d2
PillouMatou <vincent.armone@etu.univ-grenoble-alpes.fr>
2021-10-27 15:07:29 +0200

    fix tests

--- src/app/components/loading-user/loading-user.component.spec.ts
+++ src/app/components/loading-user/loading-user.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadingUserComponent } from './loading-user.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {TranslateModule} from "@ngx-translate/core";
 
 describe('LoadingUserComponent', () => {
   let component: LoadingUserComponent;
@@ -8,7 +10,8 @@ describe('LoadingUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoadingUserComponent ]
+      declarations: [ LoadingUserComponent ],
+      imports: [RouterTestingModule, TranslateModule.forRoot()]
     })
     .compileComponents();
   }));
